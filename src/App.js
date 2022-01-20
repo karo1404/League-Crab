952f89b82a5060fe327a3d46cfbb95a598df3523
Karol Jażdrzyk <karol1404@vp.pl>
2022-01-20 16:39:54 +0100

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import teemo from './image/Teemo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={teemo} className="App-logo" alt="teemo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Tutaj <i>coś</i> powstanie.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
